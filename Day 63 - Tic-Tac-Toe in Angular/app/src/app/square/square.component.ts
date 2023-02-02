54a2673acdf8d6edd0d2ccf74819f7846f2cd19d
gapigo <x.gabriel12@gmail.com>
2023-02-02 01:55:04 -0300

    Update in day 63 - Adding broken version of the game

---
+++ Day 63 - Tic-Tac-Toe in Angular/app/src/app/square/square.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: ` <p>{{ value }}</p> `,
+  styles: [],
+})
+export class SquareComponent {
+  @Input()
+  value!: 'X' | 'O';
+}
